34f0268924b6d9f47c494b3b2e55e49b5d7f5619
Badza <badzagua.luka@gmail.com>
2025-10-15 21:00:52 +0200

    CC

--- src/main/kotlin/com/project1/config/CustomRepositoryRestConfigurer.kt
+++ src/main/kotlin/com/project1/config/CustomRepositoryRestConfigurer.kt
@@ -3,7 +3,6 @@ package com.project1.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
-import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 
 @Configuration
